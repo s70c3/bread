ba14bcd6fc12ef2987ddd790da92eec557aeaec1
s70c3 <mari.s70c3@gmail.com>
2024-05-23 13:04:24 +0200

    Integrating adding items and managing items in one page.

--- frontend/pages/6_Управление_камерами.py
+++ frontend/pages/6_Управление_камерами.py
@@ -47,3 +47,27 @@ elif action == 'Удалить':
             st.write('Не удалось удалить')
             st.write('Данные ошибки:')
             st.write(response.text)
+
+
+
+st.markdown("Добавление новой камеры")
+st.sidebar.header("Добавление новой камеры")
+
+with st.form(key="bread_form"):
+    text_input = st.text_input("Введите название камеры")
+
+    address = st.text_input("Введите rtsp-адрес камеры")
+
+    description = st.text_input("Введите описание камеры")
+
+    submit_button = st.form_submit_button(label='Отправить')
+
+
+if submit_button:
+    # Send the images to FastAPI
+    response = requests.post('http://backend:8543/camera/',
+                             json={"name": text_input, "rtsp_stream" : address, "description" : description})
+    if response.status_code == 200:
+        st.write('Изображения загружены успешно.')
+    else:
+        st.write('Не удалось загрузить изображения.')
--- frontend/pages/7_Управление_продуктами.py
+++ frontend/pages/7_Управление_продуктами.py
@@ -52,3 +52,35 @@ if data:
                 st.write(response.text)
 else:
     st.write('Продукты в базе нет.')
+
+
+
+st.markdown("Добавить нового продукта")
+st.sidebar.header("Добавление нового продукта")
+
+with st.form(key="bread_form"):
+    text_input = st.text_input("Введите название продукта")
+
+    uploaded_files = st.file_uploader("Выберите изображения", accept_multiple_files=True)
+
+    # Convert the images to base64
+    images_base64 = []
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        base64_str = base64.b64encode(bytes_data).decode()
+        # st.image(bytes_data)
+        images_base64.append(base64_str)
+
+    submit_button = st.form_submit_button(label='Подтвердить')
+
+if submit_button:
+    print(images_base64)
+    # Send the images to FastAPI
+    response = requests.post('http://backend:8543/bread/',
+                             json={"name": text_input, "photos": images_base64})
+    if response.status_code == 200:
+        st.write('Изображения успешно загружены')
+    else:
+        st.write('Не удалось загрузить изображения')
+        print(response.status_code)
+        print(response.text)
